68400a95e787ccbb5bff9d81f215b836148ad9b3
Bilge Yaşar <1700003134@STU.IKU.EDU.TR>
2021-01-23 18:29:15 +0300

    20 ocak 2020

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor
+        public MyList() //ctor diyince
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            //referans nosu gitmesin diye tempArray items'in referansını tutuyo
+            T[] tempArray = items;
+            //items.Length eleman sayısını verir
+            items = new T[items.Length+1];  //mevcut eleman sayımı 1 arttır
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;  //Listten geldi
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { /*
+            //stack            //heap
+            string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+            isimler = new string[5]; //new diyince yeni adres oluşur boş array gelir
+            isimler[4] = "İlker";
+            Console.WriteLine(isimler[4]);   */
+
+            // Console.WriteLine(isimler[0]); //boş geliyor Engin ama geliyor
+            //Yeni eleman eklerken datadan geldiğinden sonradan ekleme yapamadığımız için
+            //array değil collections kullanıyoruz. 4 elemanlı array'i 5 yapamıyoruz.
+            //PS: Bellekte stack ve heap var referans tipler ikisini değer tipler sadece stack
+            //Değişken kısmı stackte mesela isimler.
+            //new dediğimiz an heap'te 4 elemanlı bir yer oluştur demek
+            //isimler adresi 101 ise isimler= new string[5] diyince yeni adres oluyo(102).
+            //isimler[4]="İlker" 102 idi. isimlerin adresi 102 oldu.
+
+
+            List<string> isimler2 = new List<string> {"Engin","Murat","Kerem","Halil"};
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);        
+            Console.WriteLine(isimler2[0]);
+
+
+            Console.ReadKey();
+        }
+    }
+}
